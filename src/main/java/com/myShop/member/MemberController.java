276a596bbcadfffa0fb964482727b4a8dd4d026d
unso99 <unso99@naver.com>
2024-04-16 00:49:02 +0900

    Refactor: 로그인 시 dto 반환 추가

--- src/main/java/com/myShop/member/MemberController.java
+++ src/main/java/com/myShop/member/MemberController.java
@@ -43,6 +43,7 @@ public class MemberController {
             UsernamePasswordAuthenticationToken authToken =
                     new UsernamePasswordAuthenticationToken(dto.getEmail(), dto.getPassword());
             authManager.authenticate(authToken);
+            response.setDto(service.getMember(dto));
         } catch (Exception e) {
             response.setError(e.getMessage());
             return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
